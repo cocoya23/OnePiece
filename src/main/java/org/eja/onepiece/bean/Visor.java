e37a16fa8d1eca29524cba2282b6aac32e882da3
quique <quique@192.168.0.105>
2014-03-23 16:07:25 -0600

    Final with prettyfaces and with videos into app

--- src/main/java/org/eja/onepiece/bean/Visor.java
+++ src/main/java/org/eja/onepiece/bean/Visor.java
@@ -8,6 +8,7 @@ package org.eja.onepiece.bean;
 
 import java.io.File;
 import java.util.regex.Matcher;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
@@ -19,34 +20,12 @@ import org.eja.onepiece.pojo.ArchivoVideo;
  * @author quique
  */
 @ManagedBean
-@RequestScoped
+@ApplicationScoped
 public class Visor {
 
-    private String rutaVideo;
     private String tipo;
     private String capitulo;
     
-    public Visor() {
-        
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-
-        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
-        tipo = request.getParameter("tipo");
-        capitulo = request.getParameter("capitulo");
-        
-        
-        rutaVideo = tipo+"/"+capitulo+".mp4";        
-        
-    }
-
-    public String getRutaVideo() {
-        return rutaVideo;
-    }
-
-    public void setRutaVideo(String rutaVideo) {
-        this.rutaVideo = rutaVideo;
-    }
-
     public String getTipo() {
         return tipo;
     }
